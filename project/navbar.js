a6528812ef541d96acc07233921fe36c80016182
Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2024-10-29 06:56:22 +0530

    first commit

---
+++ project/navbar.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.getElementById('burgerMenu');
+const navLinks = document.getElementById('navLinks');
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+});
